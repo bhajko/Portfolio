1deb17796e965ac0f166f3e9c1add9b47c6539a6
bhajko <bhajko@gmail.com>
2019-04-28 15:47:04 +0200

    navbar toggle works

--- src/components/header.js
+++ src/components/header.js
@@ -12,13 +12,18 @@ const Header = () => {
         }
     `)
 
+    const toggleNavbar = () => {
+        document.querySelector('.navbar-menu').classList.toggle('is-active')
+        document.querySelector('.navbar-burger').classList.toggle('is-active')
+    }
+
     return (
         <header>
             <nav className="navbar" role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
                     <Link className="navbar-item" to="/">{data.site.siteMetadata.author}</Link>
 
-                    <div className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasic">
+                    <div className="navbar-burger burger" onClick={toggleNavbar} aria-label="menu" aria-expanded="false" data-target="navbarBasic">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
