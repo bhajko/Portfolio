0bd3fe20fefb5de018475fe2b9ef77f288395203
bhajko <bhajko@gmail.com>
2019-07-31 20:40:32 +0200

    manifest, offline plugin and favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,18 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+    "gatsby-plugin-offline",
   ],
 }
