580c5e0f8cc542d86805505ced7ec07274553510
bhajko <bhajko@gmail.com>
2019-08-04 10:20:32 +0200

    404 page test

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const NotFound = () => {
-    return (
-        <Layout>
-            <SEO title="404: Not found" />
-            <div className="container">
-                <h1>Page not found</h1>
-                <p><Link to="/">Head home</Link></p>
-            </div>
-        </Layout>
-    )
+  const data = useStaticQuery(graphql`
+    query {
+      robot: file(relativePath: { eq: "robot.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1600) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter has-text-centered">
+            <h1 className="title is-1">Page not found</h1>
+            <Img fluid={data.robot.childImageSharp.fluid} />
+            <p>
+              <Link to="/">Head home</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default NotFound
